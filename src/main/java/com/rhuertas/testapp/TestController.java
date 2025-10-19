d72a078317c3270d6b5999329d9d1efe46576040
Raul Huertas <rax20037@gmail.com>
2025-10-18 22:20:07 -0500

    loading yml files

--- src/main/java/com/rhuertas/testapp/TestController.java
+++ src/main/java/com/rhuertas/testapp/TestController.java
@@ -1,15 +1,17 @@
 package com.rhuertas.testapp;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 @RequestMapping("/api")
 public class TestController {
-
+    @Value("${app.author}")
+    private String author;
     @GetMapping("/test")
     public String testEndpoint() {
-        return "hi, you can do this!";
+        return "hi, you can do this!"+ author;
     }
 }
