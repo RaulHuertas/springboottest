57d09b8910642b0ae1874ef36454e63ac729f38f
Raul Huertas <rax20037@gmail.com>
2025-10-19 19:38:45 -0500

    adding the logger

--- src/main/java/com/rhuertas/testapp/TestController.java
+++ src/main/java/com/rhuertas/testapp/TestController.java
@@ -4,13 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.util.List;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 @RestController
 @RequestMapping("/api")
 public class TestController {
@@ -20,6 +22,12 @@ public class TestController {
     @Value("${app.author}")
     private String author;
 
+
+    Logger logger = LogManager.getLogger(TestController.class);
+
+    TestController() {
+        logger.info("TestController(by rax) initialized");
+    }
 
     @GetMapping("/test")
     public String testEndpoint() {
